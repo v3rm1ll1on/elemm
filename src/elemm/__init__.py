cda86fcb56efc8ef318519a9b2fe07c300686909
V3rm1ll1on <v3rm1ll1on@proton.me>
2026-04-22 00:17:54 +0200

    Release: v0.6.0

--- src/elemm/__init__.py
+++ src/elemm/__init__.py
@@ -2,5 +2,5 @@ from .base import AIProtocolManager
 from .fastapi import FastAPIProtocolManager, Elemm
 from .models import AIAction, ActionParam
 
-__version__ = "0.4.1"
+__version__ = "0.6.0"
 __all__ = ["Elemm", "AIProtocolManager", "FastAPIProtocolManager", "AIAction", "ActionParam"]
