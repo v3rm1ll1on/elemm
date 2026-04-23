4a5d75a2184be9948652d9fef55855ede3e4fd3a
V3rm1ll1on <v3rm1ll1on@proton.me>
2026-04-23 20:56:51 +0200

    chore: bump version to 0.9.1

--- src/elemm/__init__.py
+++ src/elemm/__init__.py
@@ -2,6 +2,6 @@ from .core.manager import BaseAIProtocolManager
 from .integrations.fastapi.manager import FastAPIProtocolManager, Elemm
 from .core.models import AIAction, ActionParam
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __all__ = ["Elemm", "FastAPIProtocolManager", "BaseAIProtocolManager", "AIAction", "ActionParam"]
 
