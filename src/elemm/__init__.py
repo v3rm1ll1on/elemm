334f681208bb5f241e660fcf349acad64f81349e
V3rm1ll1on <v3rm1ll1on@proton.me>
2026-04-23 16:55:44 +0200

    bump version to 0.8.0

--- src/elemm/__init__.py
+++ src/elemm/__init__.py
@@ -5,5 +5,5 @@ from .models import AIAction, ActionParam
 # Alias for backward compatibility
 AIProtocolManager = FastAPIProtocolManager
 
-__version__ = "0.5.4"
+__version__ = "0.8.0"
 __all__ = ["Elemm", "AIProtocolManager", "FastAPIProtocolManager", "BaseAIProtocolManager", "AIAction", "ActionParam"]
